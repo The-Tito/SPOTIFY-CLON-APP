1d1f254acd624eabcd320297942d95871cc23ede
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2025-11-18 04:52:40 -0600

    exposing SECRETS just for demostrative porpuses

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+import { spotify_environments } from "./environment.local";
+
+export const environment = {
+    API_URL: 'https://api.spotify.com/v1',
+    AUTH_API_URL: 'https://accounts.spotify.com/api/token',
+    CLIENT_ID: '49bbab7e08d740abaf6786727c908258',
+    CLIENT_SECRET: '0c1f3b4a914c4a0f9ce875a85654ae26'
+};
